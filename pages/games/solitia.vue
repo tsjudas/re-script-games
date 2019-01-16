1a3581a722858d4181ba0236c758cd98d2715ecb
TSjudas <tak72006@gmail.com>
2019-01-16 09:15:07 +0900

    mod solitia

--- pages/games/solitia.vue
+++ pages/games/solitia.vue
@@ -20,7 +20,7 @@
             ドラッグしてカードを動かす
           </h2>
           <h3 class="content">
-            AUTOボタンで自動送り
+            <s>AUTOボタンで自動送り</s>まだだめ
           </h3>
         </div>
       </div>
@@ -28,9 +28,11 @@
     <div class="tile is-parent is-vertical">
       <div class="tile is-child is-vertical">
         <div class="field">
-          <input class="is-checkradio has-no-border is-info" id="exampleRadioInline1" type="radio" name="exampleRadioInline" checked="checked">
+          <input class="is-checkradio has-no-border is-info" id="exampleRadioInline1" type="radio" name="exampleRadioInline" checked="checked" value="1" v-model="nextCardNum"
+            :disabled="gameState !== 'notinit'">
           <label for="exampleRadioInline1">1枚送り</label>
-          <input class="is-checkradio has-no-border is-info" id="exampleRadioInline2" type="radio" name="exampleRadioInline">
+          <input class="is-checkradio has-no-border is-info" id="exampleRadioInline2" type="radio" name="exampleRadioInline" value="3" v-model="nextCardNum"
+            :disabled="gameState !== 'notinit'">
           <label for="exampleRadioInline2">3枚送り</label>
         </div>
       </div>
@@ -38,81 +40,183 @@
         <button class="button is-info is-rounded" @click="initGame()" :disabled="gameState !== 'notinit'">Init</button>
         <button class="button is-danger is-rounded" @click="initGame()" :disabled="gameState !== 'notinit'">Reset</button>
       </div>
-      <div class="tile is-child is-vertical">
-        <div class="card">
-          <div class="card-content">
-            <div class="game-field">
-              <div class="tile is-ancestor is-vertical">
-                <div class="tile">
-                  <div class="tile is-parent">
-                    <div class="tile is-child box">
-                      <button class="button is-primary is-rounded fill-element" @click="initGame()">Next</button>
-                    </div>
-                    <div class="tile is-child box solitia-card">
-                      card
-                    </div>
+      <div class="game-field" v-if="gameState !== 'notinit'">
+        <div class="box sub-field">
+          <div style="width: 100px;">
+            <button class="button is-info is-rounded fill-element" @click="autoPlay()">AutoPlay</button>
+          </div>
+          <div class="deck-field">
+            <div class="deck">
+              <div style="width: 100px;">
+                <button class="button is-primary is-rounded fill-element" @click="nextCard()">
+                  <div v-if="deck.length > 0">
+                    Next
                   </div>
-                  <div class="tile is-parent">
-                    <div class="tile is-child box solitia-card" v-for="line in stack" :key="line.id" draggable="true"
-                          @dragstart="dragStart(line, $event)"
-                          @dragend="dragEnd()"
-                          @dragenter="dragEnter(line)"
-                          @drop="onDrop(line)">
-                      <div v-if="line.arr.length === 0">
-                        Empty
-                      </div>
-                      <div v-else>
-                        {{line.arr[line.arr.length - 1]}}
-                      </div>
-                    </div>
+                  <div v-else>
+                    Empty!
                   </div>
+                </button>
+              </div>
+            </div>
+            <div class="hand-cards">
+              <div class="solitia-card" v-for="card in showarea.arr" :key="card.suit.mark + card.num" :style="{'color': card.suit.color}">
+                <div class="solitia-card-front" :draggable="card == showarea.arr[showarea.arr.length-1]"
+                    @dragstart="dragStart(showarea, card, $event)"
+                    @dragend="dragEnd()"
+                    @dragover="dragOver(showarea)">
+                  {{card.suit.mark}}{{card.num}}
                 </div>
-                <div class="tile box">
-                  <div class="tile is-parent is-vertical box" v-for="line in field" :key="line.id">
-                    <div class="tile is-child is-1 box solitia-card" v-for="card in line.arr" :key="card.suit.mark + card.num" :style="{'color': card.suit.color}">
-                      <div v-if="card.front">
-                        {{card.suit.mark}}{{card.num}}
-                      </div>
-                      <div v-else>
-                      </div>
-                    </div>
-                  </div>
+              </div>
+              <div class="solitia-card" v-if="showarea.arr.length <= 0 && hand.arr.length > 0" :style="{'color': hand.arr[hand.arr.length-1].suit.color}">
+                <div class="solitia-card-front" v-if="hand.arr[hand.arr.length-1].front" draggable="true"
+                    @dragstart="dragStart(hand, hand.arr[hand.arr.length-1], $event)"
+                    @dragend="dragEnd()"
+                    @dragover="dragOver(showarea)">
+                  {{hand.arr[hand.arr.length-1].suit.mark}}{{hand.arr[hand.arr.length-1].num}}
                 </div>
+              </div>
+            </div>
+          </div>
+          <div class="stack-cards">
+            <div class="solitia-card" v-for="line in stack" :key="line.id"
+                  draggable="true"
+                  @dragstart="dragStart(line, line.arr[line.arr.length-1], $event)"
+                  @dragend="dragEnd(line, $event)"
+                  @dragover="dragOver(line)">
+              <div v-if="line.arr.length === 0" class="solitia-card-none">
+              </div>
+              <div v-else class="solitia-card-front" :style="{ 'color': line.arr[line.arr.length-1].suit.color }">
+                {{line.arr[line.arr.length - 1].suit.mark}}{{line.arr[line.arr.length - 1].num}}
+              </div>
+            </div>
+          </div>
+        </div>
+        <div class="box main-field">
+          <div class="main-line" v-for="line in field" :key="line.id">
+            <div class="solitia-card" v-for="card in line.arr" :key="card.suit.mark + card.num" @click="turnCard(line, card)">
+              <div v-if="card.front" class="solitia-card-front" :style="{'color': card.suit.color}" draggable="true"
+                  @dragstart="dragStart(line, card, $event)"
+                  @dragend="dragEnd()"
+                  @dragover="dragOver(line)"
+                  >
+                {{card.suit.mark}}{{card.num}}
+              </div>
+              <div v-else class="solitia-card-back">
+              </div>
+            </div>
+            <div v-if="line.arr.length <= 0" class="solitia-card">
+              <div draggable="false" class="solitia-card-none"
+                  @dragend="dragEnd()"
+                  @dragover="dragOver(line)"
+                  >
+                FREE
               </div>
             </div>
           </div>
         </div>
       </div>
-
     </div>
   </div>
 </template>
 
 <style scoped>
-  .game-field {
-    display: flex;
-    justify-content: center;
-    align-items: center;
-  }
-  .scrollable-field {
-    margin: auto;
-    max-height: 100%;
-    overflow: auto;
-  }
-  .has-img {
-    background-size: 100% 100%;
-    text-shadow: 2px 2px 1px white;
-    background-attachment: fixed;
-  }
+.game-field {
+  width: 100%;
+  display: flex;
+  flex-flow: column;
+  justify-content: center;
+  align-items: center;
+}
+.game-field * {
+  user-select: none;
+}
+.sub-field {
+  width: 100%;
+  display: flex;
+  justify-content: space-evenly;
+  align-items: center;
+}
+.main-field {
+  width: 100%;
+  height: 600px;
+  display: flex;
+  justify-content: center;
+}
+.stack-cards {
+  display: flex;
+  justify-content: center;
+  align-items: center;
+}
+.deck-field {
+  display: flex;
+  justify-content: center;
+  align-items: center;
+}
+.scrollable-field {
+  margin: auto;
+  max-height: 100%;
+  overflow: auto;
+}
+.has-img {
+  background-size: 100% 100%;
+  text-shadow: 2px 2px 1px white;
+  background-attachment: fixed;
+}
+.solitia-card {
+  min-height: 40px;
+  height: 100px;
+  width: 80px;
+  font-size: 20px;
+  border: 1px solid lightgray;
+}
+.solitia-card-front {
+  width: 100%;
+  height: 100%;
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  
+}
+.solitia-card-back {
+  width: 100%;
+  height: 100%;
+  background-color: whitesmoke;
+}
+.solitia-card-none {
+  width: 100%;
+  height: 100%;
+  background-color: lightgreen;
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  
+}
+.fill-element {
+  height: 100%;
+  width: 100%;
+}
+.spacer-width-small {
+  height: 100%;
+  width: 8px;
+}
+.main-line {
+  height: 100%;
+  display: flex;
+  flex-flow: column;
+}
+@media screen and (max-width: 500px) {
   .solitia-card {
-    height: 60pt;
-    width: 100%;
-    text-align: center;
-  }
-  .fill-element {
-    height: 100%;
-    width: 100%;
-  }
+    height: 60px;
+    width: 40px;
+    font-size: 14px;
+  }  
+}
+@media screen and (min-width: 501px) and (max-width: 720px) {
+  .solitia-card {
+    height: 80px;
+    width: 60px;
+  }  
+}
 </style>
 
 <script>
@@ -144,13 +248,18 @@
       return {
         field: [],
         deck: [],
+        hand: [],
+        showarea: [],
         stack: [],
+        nextCardNum: 1,
         gameState: 'notinit',
         timerId: null,
         time: '00:00.00',
+        point: 0,
         startedTime: null,
         endTime: null,
         draggingItem: null,
+        draggingLine: null,
       }
     },
     computed: {
@@ -160,6 +269,14 @@
         this.deck = [];
         this.field = [];
         this.stack = [];
+        this.hand = {
+          id: "hand",
+          arr: [],
+        };
+        this.showarea = {
+          id: "showarea",
+          arr: [],
+        };
         for (let i = 0; i < 4; i++) {
           for (let j = 1; j <= 13; j++) {
             this.deck.push({
@@ -169,7 +286,7 @@
             });
           }
           this.stack.push({
-            id: i,
+            id: "stack" + i,
             arr: [],
           });
         }
@@ -184,7 +301,7 @@
             arr.push(card);
           }
           this.field.push({
-            id: i,
+            id: "field" + i,
             arr: arr,
           });
         }
@@ -211,20 +328,123 @@
         }
         return array;
       },
-      dragStart(item, e) {
+      nextCard() {
+        if (this.deck.length <= 0) {
+          this.returnToDeck();
+          return;
+        }
+        if (this.showarea.arr.length > 0) {
+          while (this.showarea.arr.length > 0) {
+            const card = this.showarea.arr.shift();
+            this.hand.arr.push(card);
+          }
+        }
+        for (let i = 1; i <= this.nextCardNum; i++) {
+          const card = this.deck.pop();
+          if (card == null) {
+            break;
+          }
+          card.front = true;
+          this.showarea.arr.push(card);
+        }
+        this.point -= 10;
+      },
+      returnToDeck() {
+        while (this.showarea.arr.length > 0) {
+          const card = this.showarea.arr.shift();
+          this.hand.arr.push(card);
+        }
+        while (this.hand.arr.length > 0) {
+          const card = this.hand.arr.pop();
+          card.front = false;
+          this.deck.push(card);
+        }
+        this.point -= 500;
+      },
+      dragStart(fromLine, item, e) {
         this.draggingItem = item;
-        e.target.style.opacity = '0.4'
+        this.draggingLine = fromLine;
       },
       dragEnd() {
+        if (!this.draggingItem || !this.draggingLine || !this.targetLine) {
+          return;
+        }
+
+        if (this.canPlaceCard(this.targetLine, this.draggingItem) || this.canStackCard(this.targetLine, this.draggingItem)) {
+          this.moveCard(this.draggingLine, this.draggingItem, this.targetLine);
+        }
+
         this.draggingItem = null;
-        e.target.style.opacity = '1'
+        this.draggingLine = null;
+        this.targetLine = null;
       },
-      dragEnter(item) {
-
+      dragOver(targetLine) {
+        this.targetLine = targetLine;
       },
-      onDrop(item) {
-
-      }
+      canPlaceCard(line, card) {
+        if (!card || !line.id || (!line.id.startsWith('field'))) {
+          return false;
+        }
+        const lastCard = line.arr[line.arr.length - 1];
+        // FREEの場合
+        if (!lastCard) {
+          return card.num === 13;
+        }
+        return lastCard.suit.color != card.suit.color && lastCard.num === card.num + 1
+      },
+      canStackCard(line, card) {
+        if (!card || !line.id || !line.id.startsWith('stack')) {
+          return false;
+        }
+        const lastCard = line.arr[line.arr.length - 1];
+        // FREEの場合
+        if (!lastCard) {
+          return card.num === 1;
+        }
+        return lastCard.suit.name === card.suit.name && lastCard.num === card.num - 1
+      },
+      moveCard(fromLine, card, targetLine) {
+        let index = -1;
+        for (let i = 0; i < fromLine.arr.length; i++) {
+          if (fromLine.arr[i] == card) {
+            index = i;
+            break;
+          }
+        }
+        if (index < 0) {
+          return;
+        }
+        const moveArr = fromLine.arr.splice(index);
+        targetLine.arr = targetLine.arr.concat(moveArr);
+      },
+      turnCard(line, card) {
+        if (line.arr[line.arr.length-1] == card && !card.front) {
+          card.front = true;
+        }
+      },
+      autoPlay() {
+        debugger
+        const testArr = [];
+        if (this.showarea.arr.length > 0) {
+          testArr.push(this.showarea);
+        } else {
+          testArr.push(this.hand);
+        }
+        for (const line of this.field) {
+          testArr.push(line);
+        }
+        
+        for (const arr of testArr) {
+          if (arr.arr.length <= 0) {
+            continue;
+          }
+          for (const stackLine of this.stack) {
+            if (this.canStackCard(stackLine, arr.arr[arr.arr.length-1])) {
+              this.moveCard(arr, arr[arr.length-1], stackLine);
+            }
+          }
+        }
+      },
     },
   }
 
