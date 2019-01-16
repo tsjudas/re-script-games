1efb8fde800813a470455d0af771ebe2896ef53e
Takeshi Saito <takeshi_saito@tsjudas-machine.local>
2019-01-16 11:42:18 +0900

    fix solitia :tada:

--- pages/games/solitia.vue
+++ pages/games/solitia.vue
@@ -38,25 +38,44 @@
       </div>
       <div class="tile is-child is-vertical">
         <button class="button is-info is-rounded" @click="initGame()" :disabled="gameState !== 'notinit'">Init</button>
-        <button class="button is-danger is-rounded" @click="initGame()" :disabled="gameState !== 'notinit'">Reset</button>
+        <button class="button is-danger is-rounded" @click="resetGame()" :disabled="gameState === 'notinit'">Reset</button>
       </div>
       <div class="game-field" v-if="gameState !== 'notinit'">
+        <div class="box score-field">
+          <nav class="level is-mobile">
+            <div class="level-item has-text-centered">
+              <div>
+                <p class="heading">SCORE</p>
+                <p class="title">{{point}}</p>
+              </div>
+            </div>
+            <div class="level-item has-text-centered">
+              <div>
+                <p class="heading">Time</p>
+                <p class="title">{{time}}</p>
+              </div>
+            </div>
+          </nav>
+        </div>
         <div class="box sub-field">
           <div style="width: 100px;">
-            <button class="button is-info is-rounded fill-element" @click="autoPlay()">AutoPlay</button>
+            <button class="button is-info is-rounded fill-element" @click="autoPlay()">
+              <div class="is-mobile-only"><i class="fas fa-caret-right"></i></div>
+              <div class="is-desktop-only">AutoPlay</div>
+            </button>
           </div>
           <div class="deck-field">
             <div class="deck">
-              <div style="width: 100px;">
-                <button class="button is-primary is-rounded fill-element" @click="nextCard()">
-                  <div v-if="deck.length > 0">
-                    Next
-                  </div>
-                  <div v-else>
-                    Empty!
-                  </div>
-                </button>
-              </div>
+              <button class="button is-primary is-rounded fill-element" @click="nextCard()">
+                <div v-if="deck.length > 0">
+                  <div class="is-mobile-only"><i class="fas fa-arrow-right"></i></div>
+                  <div class="is-desktop-only">Next</div>
+                </div>
+                <div v-else>
+                  <div class="is-mobile-only"><i class="far fa-times-circle"></i></div>
+                  <div class="is-desktop-only">Empty!</div>
+                </div>
+              </button>
             </div>
             <div class="hand-cards">
               <div class="solitia-card" v-for="card in showarea.arr" :key="card.suit.mark + card.num" :style="{'color': card.suit.color}">
@@ -130,9 +149,16 @@
 .game-field * {
   user-select: none;
 }
+.score-field {
+  width: 100%;
+  display: flex;
+  justify-content: space-evenly;
+  align-items: center;
+}
 .sub-field {
   width: 100%;
   display: flex;
+  flex-flow: row wrap;
   justify-content: space-evenly;
   align-items: center;
 }
@@ -151,6 +177,9 @@
   display: flex;
   justify-content: center;
   align-items: center;
+  height: 120px;
+  margin-left: 16px;
+  margin-right: 16px;
 }
 .scrollable-field {
   margin: auto;
@@ -161,6 +190,13 @@
   background-size: 100% 100%;
   text-shadow: 2px 2px 1px white;
   background-attachment: fixed;
+}
+.hand-cards {
+  display: flex;
+  flex-flow: column;
+  justify-content: center;
+  align-items: center;
+  height: 100%;
 }
 .solitia-card {
   min-height: 40px;
@@ -205,6 +241,9 @@
   flex-flow: column;
 }
 @media screen and (max-width: 500px) {
+  .is-desktop-only {
+    display: none;
+  }
   .solitia-card {
     height: 60px;
     width: 40px;
@@ -212,6 +251,18 @@
   }  
 }
 @media screen and (min-width: 501px) and (max-width: 720px) {
+  .is-desktop-only {
+    display: none;
+  }
+  .solitia-card {
+    height: 80px;
+    width: 60px;
+  }  
+}
+@media screen and (min-width: 721px) {
+  .is-mobile-only {
+    display: none;
+  }
   .solitia-card {
     height: 80px;
     width: 60px;
@@ -220,6 +271,7 @@
 </style>
 
 <script>
+  import { toast } from "bulma-toast";
   const suit = [
     {
       name: 'heart',
@@ -242,6 +294,26 @@
       color: 'black',
     },
   ];
+  const initialState = {
+    field: [],
+    deck: [],
+    hand: [],
+    showarea: [],
+    stack: [],
+    nextCardNum: 1,
+    gameState: 'notinit',
+    timerId: null,
+    time: '00:00.00',
+    point: 0,
+    startedTime: null,
+    endTime: null,
+    draggingItem: null,
+    draggingLine: null,
+    timerId: null,
+    time: '00:00.00',
+    startedTime: null,
+    endTime: null,
+  };
 
   export default {
     data() {
@@ -260,11 +332,38 @@
         endTime: null,
         draggingItem: null,
         draggingLine: null,
+        timerId: null,
+        time: '00:00.00',
+        startedTime: null,
+        endTime: null,
       }
     },
     computed: {
     },
+    watch: {
+      gameState(val) {
+        if (val === 'gameclear') {
+          toast({
+            message: "Game Clear!!!",
+            type: "is-black",
+            position: "bottom-center",
+            duration: 10000,
+            pauseOnHover: true,
+            dismissible: true,
+            animate: { in: "heartBeat", out: "bounceOut" }
+          });
+        }
+      }
+    },
     methods: {
+      resetGame() {
+        this.stopTimeWatch();
+        const selectedMode = this.nextCardNum;
+        for (const key in initialState) {
+          this.$data[key] = initialState[key];
+        }
+        this.nextCardNum = selectedMode;
+      },
       initGame() {
         this.deck = [];
         this.field = [];
@@ -347,7 +446,7 @@
           card.front = true;
           this.showarea.arr.push(card);
         }
-        this.point -= 10;
+        this.startTimeWatch();
       },
       returnToDeck() {
         while (this.showarea.arr.length > 0) {
@@ -359,7 +458,8 @@
           card.front = false;
           this.deck.push(card);
         }
-        this.point -= 500;
+        // XXX: やたら複雑なので簡略化
+        this.point -= 100;
       },
       dragStart(fromLine, item, e) {
         this.draggingItem = item;
@@ -416,14 +516,30 @@
         }
         const moveArr = fromLine.arr.splice(index);
         targetLine.arr = targetLine.arr.concat(moveArr);
+
+        // 場札から組札への移動
+        if (fromLine.id.startsWith('field') && targetLine.id.startsWith('stack')) {
+          this.point += 10;
+        } else if ((fromLine.id.startsWith('hand') || fromLine.id.startsWith('showarea')) && targetLine.id.startsWith('stack')) {
+          // 山札から組札への移動
+          this.point += 10;
+        } else if ((fromLine.id.startsWith('hand') || fromLine.id.startsWith('showarea')) && targetLine.id.startsWith('field')) {
+          // 山札から場札への移動
+          this.point += 5;
+        } else if (fromLine.id.startsWith('stack') && targetLine.id.startsWith('field')) {
+          // 組札から場札への移動
+          this.point -= 15;
+        }
+        this.startTimeWatch();
+        this.checkClearGame();
       },
       turnCard(line, card) {
         if (line.arr[line.arr.length-1] == card && !card.front) {
           card.front = true;
         }
+        this.point += 5;
       },
       autoPlay() {
-        debugger
         const testArr = [];
         if (this.showarea.arr.length > 0) {
           testArr.push(this.showarea);
@@ -440,10 +556,45 @@
           }
           for (const stackLine of this.stack) {
             if (this.canStackCard(stackLine, arr.arr[arr.arr.length-1])) {
-              this.moveCard(arr, arr[arr.length-1], stackLine);
+              this.moveCard(arr, arr.arr[arr.arr.length-1], stackLine);
             }
           }
         }
+      },
+      startTimeWatch() {
+        if (this.startedTime) {
+          return;
+        }
+        this.startedTime = new Date().getTime();
+        this.tickTime();
+      },
+      stopTimeWatch() {
+        this.startedTime = null;
+        clearTimeout(this.timerId);
+        this.timerId = null;
+      },
+      tickTime() {
+        const now = new Date().getTime();
+        const diff = new Date(now - this.startedTime);
+
+        const millisec = diff.getMilliseconds();
+        const sec100 = Math.floor(millisec / 10);
+        const sec = diff.getSeconds();
+        const min = diff.getMinutes();
+        this.time = ('00'  + min).slice(-2) + ":" + ('00'  + sec).slice(-2) + "." + ('00'  + sec100).slice(-2);
+        this.timerId = setTimeout(this.tickTime, 10);
+      },
+      checkClearGame() {
+        for (const stackLine of this.stack) {
+          const lastCard = stackLine.arr[stackLine.arr.length - 1];
+          if (!lastCard || lastCard.num !== 13) {
+            return;
+          }
+        }
+
+        this.point += 1000;
+        this.gameState = 'gameclear';
+        this.stopTimeWatch();
       },
     },
   }
